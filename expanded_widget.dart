0d45c4f415017b8907ad17d46b305ad57c58f8b8
leo <lapyaeootharthar@gmail.com>
2020-02-26 12:37:16 +0630

    added expanded widget

---
+++ expanded_widget.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Expanded_tutorial());
+}
+
+class Expanded_tutorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Container(
+            child: Text('Sport'),
+            alignment: Alignment.center,
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+                flex: 1,
+                child: Container(
+                  child: Center(
+                    child: Text(
+                      'iFit COACH',
+                      style: TextStyle(
+                          fontSize: 28.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )),
+            Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      'Today\'s plan',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Text(
+                      'Dashboard',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    )
+                  ],
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  padding: EdgeInsets.only(left: 22.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Activity',
+                    style: TextStyle(
+                      letterSpacing: 5.0,
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.blue,
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  padding: EdgeInsets.only(left: 22.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Workouts',
+                    style: TextStyle(
+                        letterSpacing: 5.0,
+                        fontSize: 22.0,
+                        color: Colors.white),
+                  ),
+                  color: Colors.amber,
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  padding: EdgeInsets.only(left: 22.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Nutrition',
+                    style: TextStyle(
+                        letterSpacing: 5.0,
+                        fontSize: 22.0,
+                        color: Colors.white),
+                  ),
+                  color: Colors.green,
+                )),
+            Expanded(
+                flex: 2,
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  padding: EdgeInsets.only(left: 22.0),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Sleep',
+                    style: TextStyle(
+                        letterSpacing: 5.0,
+                        fontSize: 22.0,
+                        color: Colors.white),
+                  ),
+                  color: Colors.teal,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
